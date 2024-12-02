5f6c7c4fcf47b14f8a39794d87bb2ab5e81667ab
Retesh Kumar Sharma <rksharma7071@gmail.com>
2024-12-02 23:09:21 +0530

    version 0.0.2

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send("Hello World")
-})
+  res.send("Hello World");
+});
+
+app.get('/data', (req, res) => {
+  const dbPath = path.join(__dirname, "db.json");
+  fs.readFile(dbPath, "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send({ error: "Failed to read data" });
+    } else {
+      res.send(JSON.parse(data));
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
