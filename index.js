ed9e4057a6e5e33340bc4ca017bbe6919154dccb
Retesh Kumar Sharma <rksharma7071@gmail.com>
2024-12-02 23:44:55 +0530

    version 0.0.5

--- index.js
+++ index.js
@@ -23,6 +23,26 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/:id', (req, res) => {
+  const dbPath = path.join(__dirname, "db.json");
+  const userId = req.params.id; 
+
+  fs.readFile(dbPath, "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send({ error: "Failed to read data" });
+    } else {
+      const users = JSON.parse(data);
+      const user = users.find((u) => u.id === userId);
+
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({ error: "User not found" });
+      }
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
